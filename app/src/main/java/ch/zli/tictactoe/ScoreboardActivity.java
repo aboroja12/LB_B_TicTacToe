dcc27c1ae4523aa2e0bbe257ebc4328936dec979
Boroja Andrej, GHR-OSA-NEX-21 <Andrej.Boroja@swisscom.com>
2024-01-15 14:33:42 +0100

    made vibration method

--- app/src/main/java/ch/zli/tictactoe/ScoreboardActivity.java
+++ app/src/main/java/ch/zli/tictactoe/ScoreboardActivity.java
@@ -1,14 +1,44 @@
 package ch.zli.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
 
 import android.os.Bundle;
 
 public class ScoreboardActivity extends AppCompatActivity {
 
+    private TextView ScoreX;
+    private TextView ScoreO;
+
+    private Button BackToGame;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoreboard);
+        ScoreX = findViewById(R.id.ScoreX);
+        ScoreO = findViewById(R.id.ScoreO);
+        BackToGame = findViewById(R.id.backToGame);
+        Intent intent = getIntent();
+
+        int xCount = intent.getIntExtra("xCount", 0);
+        int oCount = intent.getIntExtra("oCount", 0);
+
+
+        ScoreX.setText("Score X:" + xCount);
+        ScoreO.setText("Score O:" + oCount);
+
+
+
+        BackToGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
